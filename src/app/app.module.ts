a621919b148a29bae8eac4a4596ce057a4b56ee8
Kevin Chen <ningchen227@gmail.com>
2019-09-05 11:05:57 -0500

    Made major updates to personal website

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { AboutmeComponent } from './aboutme/aboutme.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    AboutmeComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
